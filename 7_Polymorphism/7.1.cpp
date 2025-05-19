ecc59074838dabf6ae4f21b415934cd74c658323
adityawardhanm <adityawardhanm@gmail.com>
2025-05-19 21:00:01 +0530

    Auto-commit on 'master' at 2025-05-19 21:00:01:
     7_Polymorphism/7.1     | Bin 0 -> 34032 bytes
     7_Polymorphism/7.1.cpp |  16 ++++++++++++++++
     2 files changed, 16 insertions(+)

--- 7_Polymorphism/7.1.cpp
+++ 7_Polymorphism/7.1.cpp
@@ -11,3 +11,19 @@ double add(double a, double b) {
     return a + b;
 }
 
+// Templates
+
+template <typename T>
+void print(const T& value){
+    cout << value << endl;
+}
+
+
+
+int main() {
+    print(add(5, 10));
+    print(add(5.5, 10.5));
+    return 0;
+
+
+}
